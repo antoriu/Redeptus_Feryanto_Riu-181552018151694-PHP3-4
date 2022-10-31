1fae838317bf4ef42fc39d381ba167ce7484d387
antoriu <116058839+antoriu@users.noreply.github.com>
2022-10-31 05:40:42 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php
+    // belajar php
+      /*
+        berapa baris
+        komentar  
+        terserah teman2
+      */
+
+      // variabel pada php
+      // tipe data -string-
+      $nana  = "Sekolah koding";
+      $nana2 = "Bermain koding";
+
+      echo "selamat datang di $nana <br>";
+
+      //tipe data angka/number
+      // 1. Integer 2. Float
+
+      $angka  = 1000;
+      $angka2 = 3;
+      $angka3 = 2.64;
+
+      //------------ Operator Aritmatik ----------
+      // + - + / %
+
+      //nama = nilai
+      //$angka = $angka + $angka2;
+      //$angka *+ $angka2;
+
+      //------------ Math method -------------
+      //round rand(min, max) max min
+      //echo "Angka hari ini adalah ". min($angka3, $angka, $angka2);
+
+      //----------- string method ------------
+      //strlen && str_…
+?>  
+  </body>
+</html>
